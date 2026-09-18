ae504434a2c15023fa1710ae7ccbd5138da449e7
yutaizawa <yutamac2512@icloud.com>
2026-09-18 21:22:32 +0900

    FirebaseAnalytics追加

--- MoneyManagementApp/MoneyManagementApp/MoneyManagementAppApp.swift
+++ MoneyManagementApp/MoneyManagementApp/MoneyManagementAppApp.swift
@@ -1,15 +1,21 @@
-//
-//  MoneyManagementAppApp.swift
-//  MoneyManagementApp
-//
-//  Created by Engineer MacBook Air on 2026/09/06.
-//
 
 import SwiftUI
 import SwiftData
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
 
 @main
 struct MoneyManagementAppApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
